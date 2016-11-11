df3c5e016113da252d788ef4495185b057be978e
Jason Turner <jturner@liberty.com.au>
2016-11-11 15:49:33 +1100

    Updated to include authorization

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const bodyParser = require('body-parser');
 const handler = require('feathers-errors/handler');
 var storage = require('localstorage-memory');
 
+const NeDB = require('nedb');
+const service = require('feathers-nedb');
+
 // A Feathers app is the same as an Express app
 const app = feathers();
+
+// Create a NeDB instance
+const db = new NeDB({
+  filename: './data/messages.db',
+  autoload: true
+});
+
 
 // Parse HTTP JSON bodies
 app.use(bodyParser.json());
@@ -22,16 +32,23 @@ app.configure(rest());
 // Configure Socket.io real-time APIs
 app.configure(socketio());
 // Register our authentication plugin
-app.configure(authentication({ idField: 'id' }));
+var auth = app.configure(authentication({token: {
+    secret: '123456789asdfg'
+  } }));
 
 // Register our memory "users" service
-app.use('/users', localstorage({ storage: storage}));
+app.use('/users', service({ Model: db}));
 // Register a nicer error handler than the default Express one
 app.use(handler());
 
 // Register a before hook to hash passwords
 app.service('users').before({
-  create: authentication.hooks.hashPassword()
+  create: authentication.hooks.hashPassword(),
+  all: [
+    authentication.hooks.verifyToken(),
+    authentication.hooks.populateUser(),
+    authentication.hooks.restrictToAuthenticated()
+  ]
 });
 
 // Create a test user
