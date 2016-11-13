1ec6a6325928e3932114a3d44081207c579a0928
Jason Turner <jturner@brewsoftware.com.au>
2016-11-13 20:23:40 +1100

    updated Jobs reference.

--- services/jobs.js
+++ services/jobs.js
@@ -3,14 +3,14 @@ const NeDB = require('nedb');
 module.exports = {
     init: function(app) {
         // Create a NeDB instance
-        const jobsdb = new NeDB({
+        const db = new NeDB({
             filename: './data/jobs.db',
             autoload: true
         });
 
         // jobs database
         app.use('/jobs', service({
-            Model: jobsdb
+            Model: db
         }));
 
     }
