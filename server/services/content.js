520a5f0781e05ea88be5211746e9449baaa29381
Jason Turner <jturner@liberty.com.au>
2016-11-23 13:45:08 +1100

    Update to new boilerplate with authentication and roles.

--- server/services/content.js
+++
@@ -1,7 +0,0 @@
-const feathers = require('feathers');
-
-module.exports = {
-    init: function(app) {
-        app.use('/content', feathers.static(__dirname + '/../content'));
-    }
-};
---
+++ test/__test__/null_spec.js
@@ -0,0 +1,6 @@
+
+/* eslint  no-shadow: 0, no-var: 0 */
+
+describe('hang-ten', () => {
+  it('pass build', () => {});
+});
