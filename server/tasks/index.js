481bf836ede0035ab2ebcac2d3522be4769388d7
Jason Turner <jturner@brewsoftware.com.au>
2016-12-19 22:46:22 +1100

    cleanup

--- server/tasks/index.js
+++ server/tasks/index.js
@@ -2,13 +2,9 @@
 const debug = require('debug')('server:app');
 const serverRegistration = require('./server-registration');
 const heartBeat = require('./heart-beat');
-module.exports = function () { // 'function' needed as we use 'this'
+module.exports = function () {
+  // 'function' needed as we use 'this'
   debug('Config');
   const app = this;
-  // serverRegistration();
   heartBeat();
-  // app.configure(serverRegistration);
-
-  // Note: Async polling thread being called
-  // app.configure(heartBeat);
 }
