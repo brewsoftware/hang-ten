8d66203f030a965a376bed4a1482de40c31cccc4
Jason Turner <jturner@brewsoftware.com.au>
2017-01-14 13:21:31 +1100

    Configure parse on task server startup

--- server/tasks/index.js
+++ server/tasks/index.js
@@ -2,9 +2,15 @@
 const debug = require('debug')('server:app');
 const serverRegistration = require('./server-registration');
 const heartBeat = require('./heart-beat');
+const Parse = require('parse/node').Parse;
+var config = require('config');
 module.exports = function () {
   // 'function' needed as we use 'this'
-  debug('Config');
+  debug('Server Config');
+  // Configure parse server here.
+  Parse.serverURL = config.parseServer.url;
+  Parse.initialize(config.parseServer.appId, config.parseServer.key);
   const app = this;
   heartBeat();
+
 }
