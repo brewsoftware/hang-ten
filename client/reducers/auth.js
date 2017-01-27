0ef49b8ca7cf5b4c86d7108197e6c46526614032
Jason Turner <jturner@brewsoftware.com.au>
2017-01-27 22:42:57 +1100

    Added Auth action handler

---
+++ client/reducers/auth.js
@@ -0,0 +1,41 @@
+import { LOGIN, LOGOUT, LOGIN_ERROR } from '../constants/auth'
+
+const initialState = {isAuthenticated: false};
+
+export default function auth(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN:
+    {
+      var res =
+         {
+           isAuthenticated: true
+         }
+         return res;
+
+    }
+    case LOGOUT:
+    {
+      var res =
+         {
+           isAuthenticated: false,
+         }
+         return res;
+    }
+    case LOGIN_ERROR:
+    {
+      var res =
+         {
+           isError: {
+             message: action.text
+           },
+           isAuthenticated: false,
+         }
+         console.log(res);
+         return res;
+    }
+    default:
+    {
+      return state
+    }
+  }
+}
