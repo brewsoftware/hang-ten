d9f8f8973a103c68c58ad25eab6c7a33605d5cfe
Jason Turner <jturner@brewsoftware.com.au>
2017-02-01 22:52:07 +1100

    remove old auth service

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,10 +3,11 @@ import { reducer as reduxFormReducer } from 'redux-form';
 
 import { feathersServices, feathersAuthentication } from '../feathers';
 import messages from './messages';
+import auth from './auth';
 
 export default {
   routing: routerReducer, // reducers required by react-router-redux
-  auth: feathersAuthentication.reducer,
+  auth: auth,
   users: feathersServices.users.reducer,
   verifyReset: feathersServices.verifyReset.reducer,
   form: reduxFormReducer, // reducers required by redux-form
