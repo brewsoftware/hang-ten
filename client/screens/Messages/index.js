3239b73cc3eef2d7a6915aafb788c2271b19b0f6
Jason Turner <jturner@brewsoftware.com.au>
2017-02-09 22:59:01 +1100

    Update messaging service.

--- client/screens/Messages/index.js
+++ client/screens/Messages/index.js
@@ -37,18 +37,54 @@ const mapDispatchToProps = (dispatchFunction) => {
     dispatch = dispatchFunction;
     return {};
 }
-
+var subscription;
+var query;
 class Messages extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        messages: []
+      };
+    }
+
+    create(message){
+      this.state.messages.push(message);
+      console.log('Create' + message.get('title')); // This should output Mengyan
+      this.state.messages = messages;
+      setState(this.state);
+    }
+
+    remove(message){
+      this.state.messages.pop();
+      console.log('Create' + message.get('title')); // This should output Mengyan
+      this.state.messages = messages;
+      setState(this.state);
+
+    }
+
     componentDidMount() {
       dispatch(addMessage('Component mounted'));
+
+      query = new Parse.Query('Message');
+      query.descending("score");
+      query.limit(10);
+      var self = this;
+      // create,enter,update,leave,delete
+      subscription.on('create', create);
+      subscription.on('enter', create);
+      subscription.on('leave', remove);
+
+      subscription = query.subscribe();
     }
 
     componentWillUnmount() {
-
+        subscription.unsubscribe();
     }
 
     render() {
-        tableConfig.data  = this.props.message; // data taken from the props
+        // tableConfig.data = this.props.message; // data taken from the props
+        tableConfig.data = this.state.messages;
         return (
           <div>
             <AppNavBar label="Messages" screen="app/messages" />
