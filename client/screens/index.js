ee836603b6753856ee3bb20c38df8482fe2b9508
Jason Turner <jturner@brewsoftware.com.au>
2017-02-11 12:26:10 +1100

    Convert wrapper to component where object subscriptions can be handled.

--- client/screens/index.js
+++ client/screens/index.js
@@ -1,11 +1,29 @@
 
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
-const AppWrapper = (props) => (
-  <div>
-    {props.children}
-  </div>
-);
+class AppWrapper extends Component {
+  componentWillMount(){
+    // check parse user, if logged in then redirect to app page
+
+  }
+  componentWillUnmount(){
+    // unsubscribe from parse updates
+
+  }
+  componentWillReceiveProps(nextProps) {
+    
+//    if (!this.props.isAuthenticated && nextProps.isAuthenticated) { // true after successful submit
+//      this.props.handleRedirect();
+//    }
+  }
+
+  render() {
+    return (<div>
+      {this.props.children}
+    </div>);
+
+  }
+}
 
 AppWrapper.propTypes = {
   children: PropTypes.any,
