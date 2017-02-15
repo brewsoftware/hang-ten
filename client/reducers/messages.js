7b3807c7844fc9c483f3af393f67636da3404b65
Jason Turner <jturner@brewsoftware.com.au>
2017-02-15 22:43:27 +1100

    updated store logic.

--- client/reducers/messages.js
+++ client/reducers/messages.js
@@ -1,32 +1,35 @@
+
 import { MESSAGES_ADD, MESSAGES_REFRESH, MESSAGES_DELETE, MESSAGES_UPDATE} from '../constants/messages'
 //, INSERT_MESSAGE, DELETE_MESSAGE, EDIT_MESSAGE, UPDATE_MESSAGE, COMPLETE_MESSAGE, COMPLETE_ALL, CLEAR_COMPLETED }
 
 const initialState = []
-
+function convertObject(obj){
+  let newobj = {};
+  newobj.score = obj.get('score');
+  newobj.text = obj.get('test');
+  newobj.id = obj.id;
+  return newobj;
+}
 export default function MESSAGES(state = initialState, action) {
     switch (action.type) {
     case 'MESSAGES_REFRESH':
-    return action.data;
-/*      return action.data.map(function(obj){
-        let newobj = {};
-        newobj.score = obj.get('score');
-        newobj.text = obj.get('test');
-        return newobj;
-      });*/
+      var data=  action.data.map(function(obj){
+        return convertObject(obj);
+      });
+      return data;
     case MESSAGES_ADD:
+      var newObj = convertObject(obj);
       return [
-        action.data,
+        newObj,
         ...state
       ]
     case MESSAGES_DELETE:
       return state.filter(MESSAGE =>
-        MESSAGE._id !== action.id
+        MESSAGE.id !== action.data.id
     )
     case MESSAGES_UPDATE:
       return state.map(MESSAGE =>
-        MESSAGE._id === action.MESSAGE._id ?
-          action.MESSAGE :
-          MESSAGE
+        MESSAGE.id === action.data.id ? convertObject(action.data) : MESSAGE
       )
     default:
       return state
