df5775c6af714087c052f5fe1bd45e5da39b47aa
Jason Turner <jturner@liberty.com.au>
2017-03-04 17:54:28 +1100

    Update with example development config.

--- config/development.js
+++ config/development.js
@@ -12,4 +12,28 @@ module.exports = {
   database: {
     path: path.join(root, 'data-dev'),
   },
+  server: {
+    host: 'localhost',
+    port: '3030',
+  },
+  auth: {
+    token: {
+      secret: 'xxxxxxxx',
+    },
+    local: {},
+  },
+  parseServer: {
+    appId: 'xxxxxxxx',
+    key: 'TBA',
+    url: 'https://parse.buddy.com/apps',
+    name: 'Tasker'
+  },
+  authEmails: {
+    providers: {
+      postmark: {
+        fromEmail: '...@...com', // must be verified with postmarkapp.com
+        postmarkApiToken: 'xxxxxx', // our account token
+      },
+    },
+  },
 };
